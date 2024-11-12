c9becac8adb742b83715076574d03ee06533e6e8
noticesax <anatayyasongotelu@gmail.com>
2024-11-12 20:51:10 +0700

    merlinx: initial a15 twrp device tree
    Signed-off-by: noticesax <anatayyasongotelu@gmail.com>

---
+++ omni_merlinx.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from merlinx device
+$(call inherit-product, device/xiaomi/merlinx/device.mk)
+
+PRODUCT_DEVICE := merlinx
+PRODUCT_NAME := omni_merlinx
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 9
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="halcyon_merlinx-user 15 AP3A.240905.015.A2 eng.buxx.00000000.000000 dev-keys"
+
+BUILD_FINGERPRINT := Redmi/merlin/merlin:11/RP1A.200720.011/V12.5.4.0.RJOMIXM:user/release-keys
